533d9a471fe90ef224f12ea3bfa61886f634f013
Yash708262 <yash708262@gmail.com>
2025-11-09 11:42:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 var isDate = function (input) {
-  // If it's already a Date object, check if it's valid
-  if (input instanceof Date) {
+  // If input is already a Date object
+  if (Object.prototype.toString.call(input) === "[object Date]") {
     return !isNaN(input.getTime());
   }
 
-  // If it's a string or number, try to parse it as a date
-  if (typeof input === "string" || typeof input === "number") {
-    const parsedDate = new Date(input);
-    return !isNaN(parsedDate.getTime());
-  }
-
-  // For all other types (objects, arrays, etc.), return false
-  return false;
+  // Try to parse anything else (string, number, etc.)
+  const parsed = new Date(input);
+  return !isNaN(parsed.getTime());
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
 const result = isDate(input);
 alert(result);
+
